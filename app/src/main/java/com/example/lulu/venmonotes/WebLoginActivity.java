52dd79efb57d20bf32c6f266f6af631657b0e254
honolulu413 <honolulu413beiyong@gmail.com>
2015-07-09 17:38:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lulu/venmonotes/WebLoginActivity.java
@@ -0,0 +1,78 @@
+package com.example.lulu.venmonotes;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+public class WebLoginActivity extends ActionBarActivity {
+    private WebView webView;
+    private static final String TAG = "WEBVIEW";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webLogin);
+        webView = (WebView) findViewById(R.id.webView1);
+
+        webView.getSettings().setJavaScriptEnabled(true);//设置使用够执行JS脚本
+        webView.getSettings().setBuiltInZoomControls(true);//设置使支持缩放
+//      webView.getSettings().setDefaultFontSize(5);
+
+        webView.loadUrl("https://api.venmo.com/v1/oauth/authorize?client_id=2743&scope=make_payments");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.d(TAG, url);
+                if (url.contains("lulu")) return true;
+                // TODO Auto-generated method stub
+                view.loadUrl(url);// 使用当前WebView处理跳转
+                return true;//true表示此事件在此处被处理，不需要再广播
+            }
+
+            @Override   //转向错误时的处理
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                // TODO Auto-generated method stub
+                Toast.makeText(WebLoginActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override   //默认点回退键，会退出Activity，需监听按键操作，使回退在WebView内发生
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
