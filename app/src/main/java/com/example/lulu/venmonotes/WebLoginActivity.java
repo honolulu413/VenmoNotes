8bc10903722d5a68c508adf372d0f90d3989c2c9
JoeJoe1989 <josephzhou2011@gmail.com>
2015-07-09 19:53:14 -0400

    JOSEPHDDD

--- app/src/main/java/com/example/lulu/venmonotes/WebLoginActivity.java
+++ app/src/main/java/com/example/lulu/venmonotes/WebLoginActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 
 public class WebLoginActivity extends ActionBarActivity {
     private WebView webView;
-    private static final String TAG = "SSDD";
+    private static final String TAG = "SSDDDDDDD";
 
     //TTTTTTTTTTT
     @Override
