b792bc4896c4b2a707f296a38393507c6750f6a6
honolulu413 <honolulu413beiyong@gmail.com>
2015-07-09 20:28:23 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/com/example/lulu/venmonotes/WebLoginActivity.java

--- app/src/main/java/com/example/lulu/venmonotes/WebLoginActivity.java
+++ app/src/main/java/com/example/lulu/venmonotes/WebLoginActivity.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 public class WebLoginActivity extends ActionBarActivity {
     private WebView webView;
     private static final String TAG = "SSDDDDDDDfffffff";
-
     
     //TTTTTTTTTTT
     @Override
