20b7baed0b695f2e80495d83f7124321e734e6d3
JoeJoe1989 <josephzhou2011@gmail.com>
2015-07-17 19:53:39 -0400

    filter

--- app/src/main/java/com/example/lulu/venmonotes/HomePageActivity.java
+++ app/src/main/java/com/example/lulu/venmonotes/HomePageActivity.java
@@ -1,16 +1,23 @@
 package com.example.lulu.venmonotes;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.view.View;
+import android.widget.Button;
+
 
 import java.util.ArrayList;
 
@@ -21,17 +28,64 @@ public class HomePageActivity extends ActionBarActivity{
     public static final String ACCESS_TOKEN = "com.example.lulu.HomePageActivity.accessToken";
     private static final String TAG = "HOME";
     private ArrayList<Transaction> mTransactions;
-    private String mAction;
-    private String mDate;
+    private String mAction = null;
+    private String mDate = null;
+    private Button buttonFilter;
+    public static final CharSequence[] options = {"pay", "charge"};
+    public HomePageFragment fragment;
+    public String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        String token = getIntent().getStringExtra(ACCESS_TOKEN);
+        token = getIntent().getStringExtra(ACCESS_TOKEN);
         Log.d(TAG, token);
 
-        new FetchTransactions().execute(token);
+        FragmentManager fm = getSupportFragmentManager();
+        fragment = (HomePageFragment)fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = new HomePageFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+
+         new FetchTransactions().execute(token);
+
+        buttonFilter = (Button) findViewById(R.id.filterButton);
+        buttonFilter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //DO SOMETHING! {RUN SOME FUNCTION ... DO CHECKS... ETC}
+                filterAction();
+            }
+        });
+
+    }
+
+    private void filterAction() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Filter");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // The 'which' argument contains the index position
+                // of the selected item
+                switch (which) {
+                    case 0:
+                          mAction = "pay";
+                          fragment.mTransactions.clear();
+                         break;
+                    case 1:
+                        mAction = "charge";
+                        break;
+
+                }
+                new FetchTransactions().execute(token);
+//                fragment.updateUI();
+
+            }
+        });
+        builder.create().show();
     }
 
     @Override
@@ -65,7 +119,11 @@ public class HomePageActivity extends ActionBarActivity{
 
         @Override
         protected void onPostExecute(ArrayList<Transaction> result) {
-            Log.d(HttpService.TAG, "" + result);
+//            mTransactions = result;
+            fragment.mTransactions = result;
+            fragment.updateUI();
+
+
         }
 
     }
