18315db8ef318cb2ffb5fd2f21a55ed13a797f00
JoeJoe1989 <josephzhou2011@gmail.com>
2015-08-18 19:40:28 -0400

    event fragment

--- app/src/main/java/com/example/lulu/venmonotes/EventFragment.java
+++ app/src/main/java/com/example/lulu/venmonotes/EventFragment.java
@@ -1,9 +1,28 @@
 package com.example.lulu.venmonotes;
 
+import android.app.Activity;
 import android.app.usage.UsageEvents;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
@@ -12,8 +31,16 @@ import java.util.UUID;
  */
 public class EventFragment extends Fragment {
     public static final String EXTRA_EVENT_DATE = "com.eventDate";
-    private Event mEvent;
+    private static final int REQUEST_PHOTO = 1;
+    private static final String DIALOG_IMAGE = "image";
 
+    private Event mEvent;
+    private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
+    private EditText mTitleField;
+    private TextView mDateFiled;
+
+    private CheckBox mSolvedCheckBox;
 
     public static EventFragment newInstance(Date eventDate) {
         Bundle args = new Bundle();
@@ -31,4 +58,116 @@ public class EventFragment extends Fragment {
         mEvent = EventLab.get(getActivity()).getEvent(eventDate);
     }
 
+    private String getDateString(Date date) {
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        return df.format(date).replaceAll("T.+", "");
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_event, parent, false);
+
+        mDateFiled = (TextView) v.findViewById(R.id.event_date);
+        mDateFiled.setText(getDateString(mEvent.getDate()));
+
+        mPhotoButton = (ImageButton) v.findViewById(R.id.crime_imageButton);
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), EventCameraActivity.class);
+                startActivityForResult(i, REQUEST_PHOTO);
+            }
+        });
+
+
+        mPhotoView = (ImageView) v.findViewById(R.id.crime_imageView);
+        mPhotoView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Photo p = mEvent.getPhoto();
+                if (p == null)
+                    return;
+
+                FragmentManager fm = getActivity()
+                        .getSupportFragmentManager();
+                String path = getActivity()
+                        .getFileStreamPath(p.getFilename()).getAbsolutePath();
+                ImageFragment.newInstance(path)
+                        .show(fm, DIALOG_IMAGE);
+            }
+        });
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.setText(mEvent.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(
+                    CharSequence c, int start, int before, int count) {
+                mEvent.setTitle(c.toString());
+            }
+
+            public void beforeTextChanged(
+                    CharSequence c, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            public void afterTextChanged(Editable c) {
+                // This one too
+            }
+        });
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.event_solved);
+        mSolvedCheckBox.setChecked(mEvent.isSettled());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Set the crime's solved property
+                mEvent.setSettled(isChecked);
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_PHOTO) {
+            // Create a new Photo object and attach it to the crime
+            String filename = data
+                    .getStringExtra(EventCameraFragment.EXTRA_PHOTO_FILENAME);
+            if (filename != null) {
+                Photo p = new Photo(filename);
+                mEvent.setPhoto(p);
+                showPhoto();
+            }
+        }
+    }
+
+    private void showPhoto() {
+        // (Re)set the image button's image based on our photo
+        Photo p = mEvent.getPhoto();
+        BitmapDrawable b = null;
+        if (p != null) {
+            String path = getActivity()
+                    .getFileStreamPath(p.getFilename()).getAbsolutePath();
+            b = PictureUtils.getScaledDrawable(getActivity(), path);
+        }
+        mPhotoView.setImageDrawable(b);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+//        CrimeLab.get(getActivity()).saveCrimes();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        PictureUtils.cleanImageView(mPhotoView);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showPhoto();
+    }
 }
